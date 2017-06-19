676267ddfb133119ca7a88163f687d8db5c35350
innot <innot.not@mail.ru>
2017-06-19 18:49:43 +0700

    base functions

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 
-  echo 'HELLO WORLD!!!!!!!!!!!!!!!!!';
+  echo '1!!!!!!!!!!!!!!!!!';
+
+  $botToken = '448066609:AAFed17GD1yws3wV3AB4it_rWSt-yMdi6Ck';
+  $webSite = 'https://api.telegram.org/bot'.$botToken;
+
+//  $webSite = 'https://tgbot.test.maderwin.com/';
+
+  $update = file_get_contents($webSite.'getupdates');
+
+  print_r($update);
+
 
 
   //
