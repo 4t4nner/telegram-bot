e96fd8118a601efc078a85719526438ee7e527f9
innot <innot.not@mail.ru>
2017-06-19 19:36:30 +0700

    base functions

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   $chatId = $updateArray['result'][0]['message']['chat']['id'];
   $string = $webSite . '/sendmessage?chat_id=' . $chatId . '&text=test';
   print_r($string);
-  file_get_contents ('https://api.telegram.org/bot448066609:AAFed17GD1yws3wV3AB4it_rWSt-yMdi6Ck/sendmessage?chat_id=304219410&text=blabla');
+  file_get_contents ($string);
 
 
 
