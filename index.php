9c15d2b952cd61c18d97b94bedcb34d84156225b
innot <innot.not@mail.ru>
2017-06-19 19:14:02 +0700

    base functions

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-  echo '2!!!!!!!!!!!!!!!!!';
+  echo '3!!!!!!!!!!!!!!!!!';
 
   $botToken = '448066609:AAFed17GD1yws3wV3AB4it_rWSt-yMdi6Ck';
   $webSite = 'https://api.telegram.org/bot'.$botToken;
@@ -8,7 +8,9 @@
 //  $webSite = 'https://tgbot.test.maderwin.com/';
 
   $update = file_get_contents ($webSite . '/getupdates');
+  print_r ($update);
   $updateArray = json_decode ($update);
+  print_r ($updateArray);
   $chatId = $updateArray['result'][0]['message']['chat']['id'];
 //  file_put_contents ($webSite . '/sendmessage?chat_id=' . $chatId . '&text=test');
   //    chat_id
